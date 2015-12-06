05ae6201a0d62313b1e6ad05abf0081a79edc8ec
Yumarx <jumarpolanco@gmail.com>
2015-12-06 19:03:06 -0400

    initial app

---
+++ FinalWebCine/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalWebCine.Startup))]
+namespace FinalWebCine
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
